31c11ae11e65f90787b2ab4e72ee64c4e0784e21
svDweck <139154103+svDweck@users.noreply.github.com>
2024-09-09 22:22:39 -0700

    Update Main Comments.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class Main {
 
+    // Prints out a message when the user chooses a pet.
     public static void adopt(Pets pet){
         System.out.println("Congratulations! You adopted " + pet.getName() + ", the " + pet.getAge() +
                 " year old " + pet.getColor() + " " + pet.getBreed());
@@ -11,12 +12,14 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        // Creating new pets
         LongHairCats Shadow = new LongHairCats(2, "Shadow", "white", 1);
         LongHairCats Garf = new LongHairCats(3, "Garf", "orange", 2);
         SmallDogs Stella = new SmallDogs(7, "Stella", "black", 1);
         SmallDogs Beverly = new SmallDogs(4, "Beverly", "tan", 2);
 
 
+        // Prints the pet selection
         System.out.println("Here are the available pets:");
         System.out.println("1: " + Shadow.adoptionPapers() +
         "\n2: " + Garf.adoptionPapers() +
@@ -25,6 +28,7 @@ public class Main {
 
         int pet = scanner.nextInt();
 
+        // Determines which pet the user selected, and prints the corresponding adoption message
         switch(pet){
             case 1:
                 adopt(Shadow);
