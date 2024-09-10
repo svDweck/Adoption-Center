9a4a7b7f60eafc25fc30c53741c4fd86d87005fa
svDweck <139154103+svDweck@users.noreply.github.com>
2024-09-09 22:23:00 -0700

    Update Pets Comments.java

--- Pets.java
+++ Pets.java
@@ -1,27 +1,32 @@
 public class Pets {
 
-    int age;
-    String name;
-    String color;
+    int age;        // The pets age
+    String name;    // The pets name
+    String color;   // The color of the pet
 
+    // Creates a new pet
     public Pets(int age, String name, String color){
         this.age = age;
         this.name = name;
         this.color = color;
     }
 
+    // Returns the pets age
     public int getAge(){
         return age;
     }
 
+    // Returns the pets name
     public String getName(){
         return name;
     }
 
+    // Returns the pets color
     public String getColor(){
         return color;
     }
 
+    // Returns the animal breed
     public String getBreed(){
         return "pet";
     }
