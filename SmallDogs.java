c462fc7cb048db7b8bbce84a52fcbbad38078cd6
svDweck <139154103+svDweck@users.noreply.github.com>
2024-09-09 22:23:15 -0700

    Update SmallDogs Comments.java

--- SmallDogs.java
+++ SmallDogs.java
@@ -2,11 +2,13 @@ public class SmallDogs extends Dogs{
 
     int breed;      // Represents the dog breed. 1: Poodle; 2: Chihuahua; Other: Mixed Breed
 
+    // Creates a new small dog
     public SmallDogs(int age, String name, String color, int breed){
         super(age, name, color);
         this.breed = breed;
     }
 
+    // Returns the breed of the dog in String form
     public String getBreed(){
 
         if(breed == 1){
@@ -18,6 +20,7 @@ public class SmallDogs extends Dogs{
         }
     }
 
+    // Returns a string with a description of the dog
     public String adoptionPapers(){
         String papers = "This " + color + " " + getBreed() + " dog's name is " + name + ", and it is " + age + " years old.";
 
