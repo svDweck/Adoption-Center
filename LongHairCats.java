cb92985479d304f006394f407c463276528b6332
svDweck <139154103+svDweck@users.noreply.github.com>
2024-09-09 22:22:07 -0700

    Update LongHairCats Comments.java

--- LongHairCats.java
+++ LongHairCats.java
@@ -2,11 +2,13 @@ public class LongHairCats extends Cats{
 
      int breed;     // Represents the cat breed. 1: Maine Coon; 2: Persian; Other: Mixed Breed
 
+    // Creates a new long haired cat
      public LongHairCats(int age, String name, String color, int breed){
          super(age, name, color);
          this.breed = breed;
      }
 
+     // Returns the breed of the cat in String form
      public String getBreed(){
 
          if(breed == 1){
@@ -18,7 +20,7 @@ public class LongHairCats extends Cats{
          }
      }
 
-
+    // Returns a string with a description of the cat
     public String adoptionPapers(){
         String papers = "This " + this.color + " " + getBreed() +  " cat's name is " + this.name + ", and it is " + this.age + " years old.";
 
